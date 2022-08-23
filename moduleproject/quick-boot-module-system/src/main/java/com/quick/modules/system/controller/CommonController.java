ee815839ae9de8b488cb7f120d3ba0b0e7f89bcd
18862700294 <1842612454@qq.com>
2022-08-24 00:00:29 +0800

    1。接口

--- moduleproject/quick-boot-module-system/src/main/java/com/quick/modules/system/controller/CommonController.java
+++ moduleproject/quick-boot-module-system/src/main/java/com/quick/modules/system/controller/CommonController.java
@@ -2,10 +2,22 @@ package com.quick.modules.system.controller;
 
 import com.github.xiaoymin.knife4j.annotations.ApiSort;
 import com.quick.common.api.vo.Result;
+import com.quick.common.constant.CommonConstant;
+import com.quick.common.exception.BusinessException;
+import com.quick.common.util.MyConvertUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
 
 
 @Slf4j
@@ -15,19 +27,120 @@ import org.springframework.web.bind.annotation.*;
 @ApiSort(1)
 public class CommonController {
 
+    /**
+     * 本地：local minio：minio 阿里：alioss
+     */
+    @Value(value="${quick.uploadType}")
+    private String uploadType;
+
 
     @GetMapping("/403")
-    @ApiOperation(value = "测试",notes = "测试")
+    @ApiOperation(value = "403页面",notes = "403页面")
     public Result<?> noAuth(){
         return Result.error("没有权限，请联系管理员授权");
     }
 
-    @GetMapping("/index")
-    @ApiOperation(value = "测试一",notes = "测试看看")
-    public String sayHello() {
-        return "index";
+
+    /**
+     * 文件上传方法
+     * @param request
+     * @param response
+     * @return
+     */
+    @PostMapping(value = "/upload")
+    @ApiOperation(value = "文件上传方法",notes = "文件上传方法")
+    public Result<?> upload(HttpServletRequest request, HttpServletResponse response) {
+        Result<?> result = new Result<>();
+        String savePath = "";
+        String bizPath = request.getParameter("biz");
+
+        //LOWCOD-2580 sys/common/upload接口存在任意文件上传漏洞
+        if (MyConvertUtils.isNotEmpty(bizPath) && (bizPath.contains("../") || bizPath.contains("..\\"))) {
+            throw new BusinessException("上传目录bizPath，格式非法！");
+        }
+
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("file");// 获取上传文件对象
+        if(MyConvertUtils.isEmpty(bizPath)){
+            if(CommonConstant.UPLOAD_TYPE_OSS.equals(uploadType)){
+                //未指定目录，则用阿里云默认目录 upload
+                bizPath = "upload";
+                //result.setMessage("使用阿里云文件上传时，必须添加目录！");
+                //result.setSuccess(false);
+                //return result;
+            }else{
+                bizPath = "";
+            }
+        }
+        if(CommonConstant.UPLOAD_TYPE_LOCAL.equals(uploadType)){
+            //update-begin-author:lvdandan date:20200928 for:修改JEditor编辑器本地上传
+            savePath = this.uploadLocal(file,bizPath);
+            //update-begin-author:lvdandan date:20200928 for:修改JEditor编辑器本地上传
+            /**  富文本编辑器及markdown本地上传时，采用返回链接方式
+             //针对jeditor编辑器如何使 lcaol模式，采用 base64格式存储
+             String jeditor = request.getParameter("jeditor");
+             if(oConvertUtils.isNotEmpty(jeditor)){
+             result.setMessage(CommonConstant.UPLOAD_TYPE_LOCAL);
+             result.setSuccess(true);
+             return result;
+             }else{
+             savePath = this.uploadLocal(file,bizPath);
+             }
+             */
+        }else{
+            //update-begin-author:taoyan date:20200814 for:文件上传改造
+            savePath = CommonUtils.upload(file, bizPath, uploadType);
+            //update-end-author:taoyan date:20200814 for:文件上传改造
+        }
+        if(MyConvertUtils.isNotEmpty(savePath)){
+            result.setMessage(savePath);
+            result.setSuccess(true);
+        }else {
+            result.setMessage("上传失败！");
+            result.setSuccess(false);
+        }
+        return result;
     }
 
 
+    /**
+     * 本地文件上传
+     * @param mf 文件
+     * @param bizPath  自定义路径
+     * @return
+     */
+    private String uploadLocal(MultipartFile mf,String bizPath){
+        try {
+            String ctxPath = uploadpath;
+            String fileName = null;
+            File file = new File(ctxPath + File.separator + bizPath + File.separator );
+            if (!file.exists()) {
+                file.mkdirs();// 创建文件根目录
+            }
+            String orgName = mf.getOriginalFilename();// 获取文件名
+            orgName = CommonUtils.getFileName(orgName);
+            if(orgName.indexOf(".")!=-1){
+                fileName = orgName.substring(0, orgName.lastIndexOf(".")) + "_" + System.currentTimeMillis() + orgName.substring(orgName.lastIndexOf("."));
+            }else{
+                fileName = orgName+ "_" + System.currentTimeMillis();
+            }
+            String savePath = file.getPath() + File.separator + fileName;
+            File savefile = new File(savePath);
+            FileCopyUtils.copy(mf.getBytes(), savefile);
+            String dbpath = null;
+            if(MyConvertUtils.isNotEmpty(bizPath)){
+                dbpath = bizPath + File.separator + fileName;
+            }else{
+                dbpath = fileName;
+            }
+            if (dbpath.contains("\\")) {
+                dbpath = dbpath.replace("\\", "/");
+            }
+            return dbpath;
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return "";
+    }
 
 }
