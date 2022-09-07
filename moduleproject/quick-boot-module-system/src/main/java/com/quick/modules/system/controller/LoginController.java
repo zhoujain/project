4b0c73d9735bb13edadf5ba117e052ef3b91096e
zhoujain <zhoujian0727@foxmail.com>
2022-09-07 17:41:53 +0800

    1.登录接口

--- moduleproject/quick-boot-module-system/src/main/java/com/quick/modules/system/controller/LoginController.java
+++ moduleproject/quick-boot-module-system/src/main/java/com/quick/modules/system/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.quick.modules.system.controller;
 
+import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
+import com.aliyuncs.exceptions.ClientException;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.quick.common.api.vo.Result;
+import com.quick.common.constant.CacheConstant;
 import com.quick.common.constant.CommonConstant;
 import com.quick.common.system.util.JwtUtil;
 import com.quick.common.system.vo.LoginUser;
@@ -15,22 +18,18 @@ import com.quick.modules.system.entity.SysDepart;
 import com.quick.modules.system.entity.SysTenant;
 import com.quick.modules.system.entity.SysUser;
 import com.quick.modules.system.model.SysLoginModel;
-import com.quick.modules.system.service.ISysDepartService;
-import com.quick.modules.system.service.ISysDictService;
-import com.quick.modules.system.service.ISysTenantService;
-import com.quick.modules.system.service.ISysUserService;
+import com.quick.modules.system.service.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 /**
  * 登录控制器
@@ -55,6 +54,8 @@ public class LoginController {
     private ISysDictService sysDictService;
     @Autowired
     private BaseCommonService baseCommonService;
+    @Autowired
+    private ISysLogService sysLogService;
 
 
     @ApiOperation("登录接口")
@@ -105,6 +106,252 @@ public class LoginController {
         //update-end--Author:wangshuai  Date:20200714  for：登录日志没有记录人员
         return result;
     }
+
+
+    @GetMapping("/user/getUserInfo")
+    public Result<JSONObject> getUserInfo(HttpServletRequest request){
+        Result<JSONObject> result = new Result<>();
+        String userName = JwtUtil.getUserNameByToken(request);
+        if (MyConvertUtils.isNotEmpty(userName)){
+            SysUser userByName = sysUserService.getUserByName(userName);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("userInfo",userByName);
+            jsonObject.put("sysAllDictItems", sysDictService.queryAllDictItems());
+            result.setResult(jsonObject);
+            result.success("");
+        }
+        return result;
+    }
+
+    /**
+     * 退出登录
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/logout")
+    public Result<Object> logout(HttpServletRequest request, HttpServletResponse response) {
+        //用户退出逻辑
+        String token = request.getHeader(CommonConstant.X_ACCESS_TOKEN);
+        if(MyConvertUtils.isEmpty(token)) {
+            return Result.error("退出登录失败！");
+        }
+        String username = JwtUtil.getUsername(token);
+        SysUser userByName = sysUserService.getUserByName(username);
+        LoginUser sysUser = new LoginUser();
+        BeanUtils.copyProperties(userByName,sysUser);
+        if(sysUser!=null) {
+            //update-begin--Author:wangshuai  Date:20200714  for：登出日志没有记录人员
+            baseCommonService.addLog("用户名: "+sysUser.getRealname()+",退出成功！", CommonConstant.LOG_TYPE_1, null,sysUser);
+            //update-end--Author:wangshuai  Date:20200714  for：登出日志没有记录人员
+            log.info(" 用户名:  "+sysUser.getRealname()+",退出成功！ ");
+            //清空用户登录Token缓存
+            redisUtil.del(CommonConstant.PREFIX_USER_TOKEN + token);
+            //清空用户登录Shiro权限缓存
+            redisUtil.del(CommonConstant.PREFIX_USER_SHIRO_CACHE + sysUser.getId());
+            //清空用户的缓存信息（包括部门信息），例如sys:cache:user::<username>
+            redisUtil.del(String.format("%s::%s", CacheConstant.SYS_USERS_CACHE, sysUser.getUsername()));
+            //调用shiro的logout
+            SecurityUtils.getSubject().logout();
+            return Result.ok("退出登录成功！");
+        }else {
+            return Result.error("Token无效!");
+        }
+    }
+
+    /**
+     * 获取访问量
+     * @return
+     */
+    @GetMapping("loginfo")
+    public Result<JSONObject> loginfo() {
+        Result<JSONObject> result = new Result<JSONObject>();
+        JSONObject obj = new JSONObject();
+        //update-begin--Author:zhangweijian  Date:20190428 for：传入开始时间，结束时间参数
+        // 获取一天的开始和结束时间
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        Date dayStart = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date dayEnd = calendar.getTime();
+        // 获取系统访问记录
+        Long totalVisitCount = sysLogService.findTotalVisitCount();
+        obj.put("totalVisitCount", totalVisitCount);
+        Long todayVisitCount = sysLogService.findTodayVisitCount(dayStart,dayEnd);
+        obj.put("todayVisitCount", todayVisitCount);
+        Long todayIp = sysLogService.findTodayIp(dayStart,dayEnd);
+        //update-end--Author:zhangweijian  Date:20190428 for：传入开始时间，结束时间参数
+        obj.put("todayIp", todayIp);
+        result.setResult(obj);
+        result.success("登录成功");
+        return result;
+    }
+
+    /**
+     * 获取访问量
+     * @return
+     */
+    @GetMapping("visitInfo")
+    public Result<List<Map<String,Object>>> visitInfo() {
+        Result<List<Map<String,Object>>> result = new Result<List<Map<String,Object>>>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.HOUR_OF_DAY,0);
+        calendar.set(Calendar.MINUTE,0);
+        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.MILLISECOND,0);
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        Date dayEnd = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_MONTH, -7);
+        Date dayStart = calendar.getTime();
+        List<Map<String,Object>> list = sysLogService.findVisitCount(dayStart, dayEnd);
+        result.setResult(MyConvertUtils.toLowerCasePageList(list));
+        return result;
+    }
+
+
+    /**
+     * 登陆成功选择用户当前部门
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "/selectDepart", method = RequestMethod.PUT)
+    public Result<JSONObject> selectDepart(@RequestBody SysUser user) {
+        Result<JSONObject> result = new Result<JSONObject>();
+        String username = user.getUsername();
+        if(MyConvertUtils.isEmpty(username)) {
+            LoginUser sysUser = (LoginUser)SecurityUtils.getSubject().getPrincipal();
+            username = sysUser.getUsername();
+        }
+        String orgCode= user.getOrgCode();
+        this.sysUserService.updateUserDepart(username, orgCode);
+        SysUser sysUser = sysUserService.getUserByName(username);
+        JSONObject obj = new JSONObject();
+        obj.put("userInfo", sysUser);
+        result.setResult(obj);
+        return result;
+    }
+
+    /**
+     * 短信登录接口
+     *
+     * @param jsonObject
+     * @return
+     */
+    @PostMapping(value = "/sms")
+    public Result<String> sms(@RequestBody JSONObject jsonObject) {
+        Result<String> result = new Result<String>();
+        String mobile = jsonObject.get("mobile").toString();
+        //手机号模式 登录模式: "2"  注册模式: "1"
+        String smsmode=jsonObject.get("smsmode").toString();
+        log.info(mobile);
+        if(MyConvertUtils.isEmpty(mobile)){
+            result.setMessage("手机号不允许为空！");
+            result.setSuccess(false);
+            return result;
+        }
+        Object object = redisUtil.get(mobile);
+        if (object != null) {
+            result.setMessage("验证码10分钟内，仍然有效！");
+            result.setSuccess(false);
+            return result;
+        }
+
+        //随机数
+        String captcha = RandomUtil.randomNumbers(6);
+        JSONObject obj = new JSONObject();
+        obj.put("code", captcha);
+        try {
+            boolean b = false;
+            //注册模板
+            if (CommonConstant.SMS_TPL_TYPE_1.equals(smsmode)) {
+                SysUser sysUser = sysUserService.getUserByPhone(mobile);
+                if(sysUser!=null) {
+                    result.error500(" 手机号已经注册，请直接登录！");
+                    baseCommonService.addLog("手机号已经注册，请直接登录！", CommonConstant.LOG_TYPE_1, null);
+                    return result;
+                }
+                b = DySmsHelper.sendSms(mobile, obj, DySmsEnum.REGISTER_TEMPLATE_CODE);
+            }else {
+                //登录模式，校验用户有效性
+                SysUser sysUser = sysUserService.getUserByPhone(mobile);
+                result = sysUserService.checkUserIsEffective(sysUser);
+                if(!result.isSuccess()) {
+                    String message = result.getMessage();
+                    if("该用户不存在，请注册".equals(message)){
+                        result.error500("该用户不存在或未绑定手机号");
+                    }
+                    return result;
+                }
+
+                /**
+                 * smsmode 短信模板方式  0 .登录模板、1.注册模板、2.忘记密码模板
+                 */
+                if (CommonConstant.SMS_TPL_TYPE_0.equals(smsmode)) {
+                    //登录模板
+                    b = DySmsHelper.sendSms(mobile, obj, DySmsEnum.LOGIN_TEMPLATE_CODE);
+                } else if(CommonConstant.SMS_TPL_TYPE_2.equals(smsmode)) {
+                    //忘记密码模板
+                    b = DySmsHelper.sendSms(mobile, obj, DySmsEnum.FORGET_PASSWORD_TEMPLATE_CODE);
+                }
+            }
+
+            if (b == false) {
+                result.setMessage("短信验证码发送失败,请稍后重试");
+                result.setSuccess(false);
+                return result;
+            }
+            //验证码10分钟内有效
+            redisUtil.set(mobile, captcha, 600);
+            //update-begin--Author:scott  Date:20190812 for：issues#391
+            //result.setResult(captcha);
+            //update-end--Author:scott  Date:20190812 for：issues#391
+            result.setSuccess(true);
+
+        } catch (ClientException e) {
+            e.printStackTrace();
+            result.error500(" 短信接口未配置，请联系管理员！");
+            return result;
+        }
+        return result;
+    }
+
+
+    /**
+     * 手机号登录接口
+     *
+     * @param jsonObject
+     * @return
+     */
+    @ApiOperation("手机号登录接口")
+    @PostMapping("/phoneLogin")
+    public Result<JSONObject> phoneLogin(@RequestBody JSONObject jsonObject) {
+        Result<JSONObject> result = new Result<JSONObject>();
+        String phone = jsonObject.getString("mobile");
+
+        //校验用户有效性
+        SysUser sysUser = sysUserService.getUserByPhone(phone);
+        result = sysUserService.checkUserIsEffective(sysUser);
+        if(!result.isSuccess()) {
+            return result;
+        }
+
+        String smscode = jsonObject.getString("captcha");
+        Object code = redisUtil.get(phone);
+        if (!smscode.equals(code)) {
+            result.setMessage("手机验证码错误");
+            return result;
+        }
+        //用户信息
+        userInfo(sysUser, result);
+        //添加日志
+        baseCommonService.addLog("用户名: " + sysUser.getUsername() + ",登录成功！", CommonConstant.LOG_TYPE_1, null);
+
+        return result;
+    }
+
 
     /**
      * 用户信息
